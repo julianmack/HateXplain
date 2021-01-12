bb79282704a9221442e241aed22ae61849e87ab4
julianmack <julianmack@hotmail.co.uk>
2021-01-12 15:30:23 +0000

    Made eraserbenchmark only print useful values

--- eraserbenchmark/rationale_benchmark/metrics.py
+++ eraserbenchmark/rationale_benchmark/metrics.py
@@ -660,7 +660,16 @@ def main():
     else:
         logging.info("No classification scores detected, skipping classification")
 
-    pprint.pprint(scores)
+    # pprint.pprint(scores)
+
+    print('\nPlausibility')
+    print('IOU F1 :', scores['iou_scores'][0]['macro']['f1'])
+    print('Token F1 :', scores['token_prf']['instance_macro']['f1'])
+    print('AUPRC :', scores['token_soft_metrics']['auprc'])
+
+    print('\nFaithfulness')
+    print('Comprehensiveness :', scores['classification_scores']['comprehensiveness'])
+    print('Sufficiency', scores['classification_scores']['sufficiency'])
 
     if args.score_file:
         with open(args.score_file, 'w') as of:
