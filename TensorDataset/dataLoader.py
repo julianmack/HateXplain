88f32baf48940b70b57d6d9cbf2234b517b6e56d
julianmack <julianmack@hotmail.co.uk>
2021-01-13 10:25:10 +0000

    Refactor to make sequences variable length

--- TensorDataset/dataLoader.py
+++ TensorDataset/dataLoader.py
@@ -6,7 +6,6 @@ import numpy as np
 from sklearn.preprocessing import LabelEncoder
 
 
-
 def custom_att_masks(input_ids):
     attention_masks = []
 
@@ -22,34 +21,72 @@ def custom_att_masks(input_ids):
         attention_masks.append(att_mask)
     return attention_masks
 
-def combine_features(tuple_data,params,is_train=False):
+def combine_features(tuple_data,params,is_train=False, batch_size=None):
     input_ids =  [ele[0] for ele in tuple_data]
     att_vals = [ele[1] for ele in tuple_data]
     labels = [ele [2] for ele in tuple_data]
-    
-   
+
+
     encoder = LabelEncoder()
-    
+
     encoder.classes_ = np.load(params['class_names'],allow_pickle=True)
     labels=encoder.transform(labels)
-    
-    input_ids = pad_sequences(input_ids,maxlen=int(params['max_length']), dtype="long", 
-                          value=0, truncating="post", padding="post")
-    att_vals = pad_sequences(att_vals,maxlen=int(params['max_length']), dtype="float", 
-                          value=0.0, truncating="post", padding="post")
-    att_masks=custom_att_masks(input_ids)
-    dataloader=return_dataloader(input_ids,labels,att_vals,att_masks,params,is_train)
+
+    dataloader=return_dataloader(
+        input_ids=input_ids,
+        labels=labels,
+        att_vals=att_vals,
+        batch_size=batch_size or params['batch_size'],
+        params=params,
+        is_train=is_train
+    )
     return dataloader
 
-def return_dataloader(input_ids,labels,att_vals,att_masks,params,is_train=False):
-    inputs = torch.tensor(input_ids)
-    labels = torch.tensor(labels,dtype=torch.long)
-    masks = torch.tensor(np.array(att_masks), dtype=torch.bool)
-    attention = torch.tensor(np.array(att_vals),dtype=torch.float)
-    data = TensorDataset(inputs,attention,masks,labels)
+def return_dataloader(input_ids,labels,att_vals,batch_size,params,is_train=False):
+    data = BasicDataset(input_ids,att_vals,labels)
     if(is_train==False):
         sampler = SequentialSampler(data)
     else:
         sampler = RandomSampler(data)
-    dataloader = DataLoader(data, sampler=sampler, batch_size=params['batch_size'])
+    dataloader = DataLoader(
+        data,
+        sampler=sampler,
+        batch_size=batch_size,
+        collate_fn=get_collate_fn(params),
+        num_workers=3,
+    )
     return dataloader
+
+def get_collate_fn(params):
+
+    def collate_fn(batch):
+        input_ids, attentions, labels = [], [], []
+        for item in batch:
+            a, b, c = item
+
+            input_ids.append(a)
+            attentions.append(b)
+            labels.append(c)
+
+        input_ids = pad_sequences(input_ids, dtype="long",
+                            value=0, truncating="post", padding="post")
+        attentions = pad_sequences(attentions, dtype="float",
+                            value=0.0, truncating="post", padding="post")
+
+        masks = torch.tensor(np.array(custom_att_masks(input_ids)), dtype=torch.bool)
+        labels = torch.tensor(labels, dtype=torch.long)
+
+        return torch.tensor(input_ids), torch.tensor(attentions), masks, labels
+    return collate_fn
+
+class BasicDataset(torch.utils.data.Dataset):
+    def __init__(self, input_ids,att_vals,labels):
+        self.inputs = input_ids
+        self.attentions = att_vals
+        self.labels = labels
+
+    def __getitem__(self, idx):
+        return self.inputs[idx], self.attentions[idx], self.labels[idx]
+
+    def __len__(self):
+        return len(self.inputs)
